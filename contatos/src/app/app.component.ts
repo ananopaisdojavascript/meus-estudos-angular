7a846dc98b1ec0f125a88ac1ceb428bdcaf1c65d
ananopaisdojavascript <ananopaisdojavascript@gmail.com>
2023-05-28 14:58:23 -0300

    Modal para apagar contatos

--- contatos/src/app/app.component.ts
+++ contatos/src/app/app.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Contact } from './services/contact';
+import { ContactService } from './services/contact.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'contatos';
+export class AppComponent implements OnInit {
+  title = 'Nossa Lista de Contatos';
+
+  contacts: Contact[] = []
+
+  modal: boolean = false;
+
+  selectedContact: Contact = {
+    id: 0,
+    nome: '',
+    telefoneFixo: '',
+    telefoneCelular: ''
+  }
+
+  constructor(private contactService: ContactService) {}
+
+  showContacts() {
+    return this.contactService.getContacts().subscribe(data => {
+      this.contacts = data;
+      console.log(data);
+    })
+  }
+
+  deleteContact(contact: Contact) {
+    this.contactService.deleteContact(contact.id).subscribe(() => {
+      const index = this.contacts.indexOf(contact)
+      this.contacts.splice(index, 1)
+    })
+  }
+
+  openModal(contact: Contact) {
+    this.selectedContact.id = contact.id
+    this.selectedContact.nome = contact.nome
+    this.selectedContact.telefoneFixo = contact.telefoneFixo
+    this.selectedContact.telefoneCelular = contact.telefoneCelular
+    console.log(contact);
+    this.modal = true;
+  }
+
+  closeModal() {
+    this.modal = false;
+    this.showContacts()
+  }
+
+  ngOnInit(): void {
+    this.showContacts()
+  }
+
+
 }
--- contatos/src/app/services/contact.service.ts
+++ contatos/src/app/services/contact.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Contact } from './contact';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ContactService {
 
-  constructor() { }
+  url: string = 'http://localhost:3000/pessoas'
+
+  constructor(private http: HttpClient) { }
+
+  getContacts(): Observable<Contact[]> {
+    return this.http.get<Contact[]>(`${this.url}`);
+  }
+
+  deleteContact(id: number): Observable<Contact[]> {
+    return this.http.delete<Contact[]>(`${this.url}/${id}`);
+  }
 }
